efdd5348db41ce6daf7be019505833be1ffb9660
raraa19 <164477909+raraa19@users.noreply.github.com>
2024-03-23 15:45:18 +0800

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,18 @@
+#buat program untuk menampilkan biodata
+
+
+nama=("Baiq Rara Sinar Arian")
+nim=("SI20230008")
+semester=("dua")
+umur=int(18)
+alamat=("Mantang Batukliang")
+lulusan=("SMAPK NW Lendang Kekah")
+
+print("=="*10)
+print("selamat datang", nama)
+print("nim anda", nim)
+print("anda semester",semester)
+print("umur anda", umur)
+print("alamat anda", alamat)
+print("lulusan sekolah", lulusan)
+print("=="*10)
